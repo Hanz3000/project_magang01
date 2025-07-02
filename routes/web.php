7a13d50e0581b853a5dd319cadb83de230cf3d42
SatriaSheva <160394682+SatriaSheva@users.noreply.github.com>
2025-07-02 11:23:24 +0700

    update edit pemasukan

--- routes/web.php
+++ routes/web.php
@@ -107,9 +107,9 @@ Route::middleware('auth')->group(function () {
     });
     Route::get('pengeluarans/export/excel', [PengeluaranController::class, 'exportExcel'])->name('pengeluarans.export.excel');
     Route::get('pengeluarans/export/csv', [PengeluaranController::class, 'exportCsv'])->name('pengeluarans.export.csv');
-    Route::put('/pengeluarans/{id}', [PengeluaranController::class, 'update'])->name('pengeluarans.update');
-    Route::get('/struks/{id}/items', [TransaksiController::class, 'getStrukItems']);
+
+    
 
     Route::get('/struks/{struk}/items', [StrukController::class, 'getItems'])->name('struks.items');
-    Route::get('/pengeluaran', [PengeluaranController::class, 'index']);
+
 });
